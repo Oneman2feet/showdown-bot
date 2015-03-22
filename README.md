da85a52f390b8ab94fd44516991a0ee4e08c90a8
Zachary Zimmerman <Oneman2feet@gmail.com>
2015-03-22 16:02:13 -0400

    selenium

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ A Programmable Pokemon Showdown Battle Bot
 
 ## Installation
 
-1. [Download PhantomJS](http://phantomjs.org/download.html) or ```brew install phantomjs```
+1. [Download Selenium](http://www.seleniumhq.org/download/) or ```pip install selenium```
 
-2. To run, execute ```phantomjs hello.js```
+2. [Download PhantomJS](http://phantomjs.org/download.html) or ```brew install phantomjs```
+
+3. Try ```python test.py``` and ```phantomjs hello.js```
--- hello.js
+++ hello.js
@@ -1,2 +1,44 @@
-console.log('Hello, world!');
-phantom.exit();
+var page = require('webpage').create();
+var showdown = 'http://play.pokemonshowdown.com/';
+var output = 'example.png';
+var username = 'zarly';
+var password = 'phantom';
+
+openPage(showdown, function() {
+    var login;
+    page.evaluate(function() {
+        login = document.querySelector("button[name='login']");
+    });
+    console.log("login: " + login);
+    eventFire(login,'click');
+    window.setTimeout(function () {
+        page.evaluate(function() {
+            console.log(document.querySelector("form").innerHTML);
+        });
+        phantom.exit();
+    }, 1000);
+});
+
+
+function openPage(url,callback) {
+    console.log("Loading URL: "+url);
+    page.open(url, function(status) {
+        if (status!=="success") {
+            console.log("Page load failed");
+            phantom.exit();
+        }
+        console.log("Page load succeeded");
+        callback();
+    });
+}
+
+
+function eventFire(el, etype){
+    if (el.fireEvent) {
+        el.fireEvent('on' + etype);
+    } else {
+        var evObj = document.createEvent('Events');
+        evObj.initEvent(etype, true, false);
+        el.dispatchEvent(evObj);
+    }
+}
