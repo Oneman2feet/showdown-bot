7863273b114178a38b506552f2b454cbed5d4c11
Zachary Zimmerman <Oneman2feet@gmail.com>
2015-03-23 09:57:28 -0400

    basic battle strategy, need switching

--- test.py
+++ test.py
@@ -1,20 +1,37 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-debug = False
+debug = True
 showdown = 'http://play.pokemonshowdown.com/'
 
 driver = webdriver.Firefox() if debug else webdriver.PhantomJS()
+wait = WebDriverWait(driver, 120)
 def main():
-    driver.set_window_size(1120, 550)
+    driver.set_window_size(1200, 800)
     driver.get(showdown)
     print driver.current_url
+    driver.implicitly_wait(3)
     login()
-    driver.quit()
+    driver.implicitly_wait(3)
+    driver.find_element_by_name('search').click()
+    print "Battle Started"
+    timer = False
+    while True: #len(driver.find_elements_by_name('closeAndMainMenu')) > 0:
+        wait.until(EC.element_to_be_clickable((By.NAME,'chooseMove')))
+        driver.find_element_by_name('chooseMove').click()
+        if not timer and len(driver.find_elements_by_name('setTimer')) > 0:
+            driver.find_element_by_name('setTimer').click()
+            timer = True
+
+    #driver.quit()
 
 
 username = 'zarly'
 password = 'phantom'
 def login():
+    wait.until(EC.element_to_be_clickable((By.NAME,'login')))
     driver.find_element_by_name('login').click()
     driver.find_element_by_name('username').send_keys(username)
     driver.find_element_by_css_selector('button[type=submit]').click()
